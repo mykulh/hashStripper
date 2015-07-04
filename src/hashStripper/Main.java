cddc780902326a16207b0f0a55a32894dc084f4d
mykulh <mykulh@linnew>
2015-07-04 21:49:21 +0100

    tidied up a bit

--- src/hashStripper/Main.java
+++ src/hashStripper/Main.java
@@ -19,27 +19,30 @@ package hashStripper;
 			}
 			else{
 				int county = 0;
-				long charCount = 0;
+				long hashCount = 0;
 				BufferedReader br = new BufferedReader(new FileReader(args[0]));
-				FileWriter fw = new FileWriter(args[0] + ".found");
-				BufferedWriter out = new BufferedWriter(fw);
+				FileWriter fw = new FileWriter(args[0] + ".hashesfound");
+				BufferedWriter hashes_out = new BufferedWriter(fw);
+				FileWriter fw = new FileWriter(args[0] + ".hashesremoved");
+				BufferedWriter other_out = new BufferedWriter(fw);
 				String line;
 				System.out.println("List loaded, checking characters....");
 				while ((line = br.readLine()) != null) {
-				if((line.startsWith("$H") || line.startsWith("$P$")) && line.length() == 34){
-					for (char ch: line.toCharArray()) {
-						if(Character.toString(ch).equals("$")){
+				if((line.startsWith("$H$") || line.startsWith("$P$")) && line.length() == 34){
+					//for (char ch: line.toCharArray()) {
+					/	if(Character.toString(ch).equals("$")){
 							out.write(line);
 							out.newLine();
-							charCount++;
+							hashCount++;
 						}
-					}
+				///	}
 					county++;
 				}
 			}
 				br.close();
-				out.close();
-				System.out.println("Finished. Lines processed: " + county + " newlines saved " + charCount + ".");
+				hashes_out.close();
+				others_out.close();
+				System.out.println("Finished. Lines processed: " + county + " newlines saved " + hashCount + ".");
 			}
 			
 		}
